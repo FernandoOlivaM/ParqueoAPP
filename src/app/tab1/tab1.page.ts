1c6eee0ddf517c53bcdece5650a79bd302ac3a4e
Derly Rodas <derly.rodas@gmail.com>
2021-11-21 19:14:07 -0600

    Reporte

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PaymentService } from '../services/payment.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  tickets: [
+    {
+      Fec_salida: Date;
+      Lugar_ID: Number;
+      Fec_Ingreso: Date;
+      Ticket_ID: String;
+      precio: Number;
+      tiempo: string;
+    }
+  ];
 
-  constructor() {}
+  constructor(private paymentService: PaymentService) {}
+  ngOnInit(): void {
+    this.search();
+  }
 
+  async search() {
+    this.tickets = await this.paymentService
+      .getAlltickts()
+      .then((data) => data.Body);
+  }
 }
